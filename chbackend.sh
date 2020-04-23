5677883e624c2183ab92789dd6f86383b8878a26
Adi Apriyanto <adyoi@users.noreply.github.com>
2020-04-23 22:10:08 +0700

    Version 2.0

---
+++ chbackend.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# yii2-webapps - Change Backend Directory
+# by adyoi
+
+while true; do
+echo ""
+echo "Yii2 Web Application"
+echo ""
+echo "Change Backend Directory"
+echo "------------------------"
+read -p "change from  : " source
+read -p "change to    : " destination
+echo "--------------"
+if mv $source $destination
+then echo "[success] /"$destination "moved !"
+else echo "[failure] /"$source "not found !"
+fi
+exit; done
